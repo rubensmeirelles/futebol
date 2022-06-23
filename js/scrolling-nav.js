0d929f2b0be9dde4130864ce83956b1598bed538
rubensmeirelles <rrubens.meirelles@gmail.com>
2022-06-23 13:46:42 -0300

    Redefinindo o projeto

---
+++ js/scrolling-nav.js
@@ -0,0 +1,32 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);      
+        
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 68)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 68
+  });
+  
+})(jQuery); // End of use strict
+
+
